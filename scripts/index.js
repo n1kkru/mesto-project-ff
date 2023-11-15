dc62b059ed94e60117cd8596a6270c444c6e1146
Nik Krupin <krupin.ns@mail.ru>
2023-11-15 23:10:38 +0300

    Добавлена логика добавления и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(title, img) {
+  const cardElem = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElem.querySelector('.card__image').src = img;
+  cardElem.querySelector('.card__image').alt = title;
+  cardElem.querySelector('.card__title').textContent = title;
+
+  cardElem.querySelector('.card__delete-button').addEventListener('click', deleteCard); 
+
+  return cardElem;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+  console.log(evt.target.parentElement)
+  evt.target.parentElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (elem) {
+  
+  cardList.append(createCard(elem.name, elem.link));
+})
